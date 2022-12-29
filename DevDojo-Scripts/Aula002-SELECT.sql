a42173679ab75c79de48b75185e32d0912f2f1a0
Wilson <wilson-gs@hotmail.com>
2022-12-29 13:19:39 -0300

    Finalizado Aula 10

--- DevDojo-Scripts/Aula002-SELECT.sql
+++ DevDojo-Scripts/Aula002-SELECT.sql
@@ -3,14 +3,21 @@
     T-SQL - TRANSACT SQL
 
     SELECT  = SELECIONE
-    * TUDO (TODAS_AS_COLUNAS)
 
-    FROM = A PARTIR DE/VINDO DE
-    Nome_da_tabela
-    
+    * = TUDO (TODAS_AS_COLUNAS)
+
+    FROM = A PARTIR DE/VINDO DE <Nome_da_tabela>
+
     WHERE = ONDE (Filtro)
-        AND = E
+    
+    AND = E
+   
 */
+
+SELECT * FROM Person.Person
+WHERE FirstName = 'Ken';
+
+-- ------------------------------------------------------------------------------
 
 SELECT  BusinessEntityID, 
         PersonType,
@@ -20,11 +27,43 @@ SELECT  BusinessEntityID,
         LastName,
         ModifiedDate -- ultima coluna sem virgula
 
-  FROM Person.Person 
- WHERE FirstName = 'Ken'
-   AND MiddleName = 'LRRGRG'
+FROM Person.Person 
+WHERE FirstName = 'Ken'
+AND MiddleName = 'LRRGRG';
 
+-- ------------------------------------------------------------------------------
+-- ORDENACAO DESC = DECRESCENTE
+-- ORDENACAO ASC = ASCENDENTE 
+-- TOP = determina a quantidade de linha a ser buscada
+-- AS - ALIASES = APELIDO DA COLUNA
 
-SELECT TOP 1000 
-       * 
-    FROM Person.Person
+SELECT --TOP 1000 
+      BusinessEntityID AS [ID PESSOA],
+      PersonType       AS [TIP PESSOA],
+      Title            AS [TITULO],
+      FirstName        AS [PRIMEIRO NOME],
+      MiddleName       AS [NOME DO MEIO],
+      LastName         AS [ULTIMO NOME]
+FROM Person.Person
+WHERE Title = 'Mr.'  -- WHERE NAO CONHECE O NOME DO APELIDO DA COLUNA
+ORDER BY [PRIMEIRO NOME] , 
+         [ULTIMO NOME] DESC;
+
+-- ------------------------------------------------------------------------------
+-- DISTINCT = REMOVE DUPLICADOS
+
+SELECT --DISTINCT
+     --TOP 1000 
+     -- BusinessEntityID AS [ID PESSOA],
+     -- PersonType       AS [TIP PESSOA],
+     -- Title            AS [TITULO],
+      FirstName          --AS [PRIMEIRO NOME]
+      MiddleName      -- AS [NOME DO MEIO],
+     -- LastName         AS [ULTIMO NOME]
+FROM Person.Person
+--WHERE Title = 'Mr.'  -- WHERE NAO CONHECE O NOME DO APELIDO DA COLUNA
+ORDER BY FirstName, 
+         MiddleName
+        -- [ULTIMO NOME] DESC;
+
+-- ------------------------------------------------------------------------------
